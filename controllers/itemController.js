9e7b22f7f06937afb414fc0215de4a6d95009c79
James Spencer <jameskcs1254@gmail.com>
2022-01-17 18:52:37 +0000

    Create read only route controllers

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,17 +2,43 @@ const Item = require('../models/item');
 
 // Item index page GET
 exports.itemIndex = (req, res) => {
-  res.send('NOT IMPLEMENTED: Item index')
+  res.render('itemIndex')
 };
 
 // Display list of all items.
-exports.itemList = (req, res) => {
-  res.send('NOT IMPLEMENTED: item list');
+exports.itemList = (req, res, next) => {
+  Item.find().sort({ name: 'asc' }).exec((err, items) =>{
+    if (err) { return next(err); }
+
+    if (items === null) {
+      const err = new Error('No rocks found');
+      err.status = 404;
+      return next(err);
+    }
+
+    res.render('itemList', { title: 'Item List', items: items })
+  })
 };
 
 // Display detail page for a specific item.
-exports.itemDetail = (req, res) => {
-  res.send('NOT IMPLEMENTED: item detail: ' + req.params.id);
+exports.itemDetail = (req, res, next) => {
+  Item.findById(req.params.id).populate('category').exec((err, itemInstance) => {
+    if (err) { return next(err); }
+
+    if (itemInstance === null) {
+      const err = new Error('Rock not found');
+      err.status = 404;
+      return next(err);
+    }
+
+    res.render('itemDetail', { 
+      title: itemInstance.name, 
+      description: itemInstance.description, 
+      category: itemInstance.category.name, 
+      price: itemInstance.price,
+      numberInStock: itemInstance.numberInStock
+    });
+  });
 };
 
 // Display item create form on GET.
