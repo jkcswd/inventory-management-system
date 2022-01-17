f077540c35f6722053332e6841d474c7e308d524
James Spencer <jameskcs1254@gmail.com>
2022-01-17 13:28:12 +0000

    Create category detail and index controllers

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,15 +1,21 @@
 const Category = require('../models/category');
 
+// Category index page with list of categories
 exports.categoryIndex = (req, res) => {
-  res.send('NOT IMPLEMENTED: category index')
-};
-
-// Display list of all categories.
-exports.categoryList = (req, res) => {
-  res.send('NOT IMPLEMENTED: category list');
+  res.render('categoryIndex');
 };
 
 // Display detail page for a specific category.
-exports.categoryDetail = (req, res) => {
-  res.send('NOT IMPLEMENTED: category detail: ' + req.params.id);
+exports.categoryDetail = (req, res, next) => {
+  Category.findById(req.params.id).exec( (err, categoryInstance) => {
+    if (err) { return next(err); }
+
+    if (categoryInstance === null) {
+      const err = new Error('Category not found');
+      err.status = 404;
+      return next(err);
+    }
+
+    res.render('categoryDetail', { title: categoryInstance.name, description:  categoryInstance.description});
+  });
 };
