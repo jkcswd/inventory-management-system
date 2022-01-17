42aba89e4caf85cd014246e2c52833d9a0bd6cd7
James Spencer <jameskcs1254@gmail.com>
2022-01-17 10:01:14 +0000

    Create Schema. Export model.

---
+++ models/item.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+
+const ItemSchema = new Schema(
+  {
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    category: {type: Schema.Types.ObjectId, ref: 'Category', required: true},
+    price: {type: Number, required: true},
+    numberInStock: {type: Number, required: true}
+  }
+);
+
+ItemSchema.virtual('url').get(() => {
+  return '/inventory/item' + this._id;
+});
+
+
+module.exports = mongoose.model('Item', ItemSchema);
